2401d92b3502860b65c0a3ba20f2d99e2014de0f
John van Dijk <john@johnv.ca>
2025-05-19 08:19:42 -0400

    Add project files.

---
+++ WIMF/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WIMF
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WIMF/ViewModels/FreezerItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using WIMF.Models;
+
+namespace WIMF.ViewModels
+{
+    internal class FreezerItemViewModel
+    {
+        public ObservableCollection<FreezerItem> FreezerItems { get; set; }
+
+        public FreezerItemViewModel()
+        {
+            FreezerItems = new ObservableCollection<FreezerItem>
+            {
+                new FreezerItem { Id = 1, Name = "Ice Cream", Description = "Vanilla ice cream", DateAdded = DateTime.Now },
+                new FreezerItem { Id = 2, Name = "Frozen Peas", Description = "Green peas", DateAdded = DateTime.Now },
+                new FreezerItem { Id = 3, Name = "Frozen Chicken", Description = "Chicken breasts", DateAdded = DateTime.Now }
+            };
+        }
+    }
+}
